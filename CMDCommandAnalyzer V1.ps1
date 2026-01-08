6d18709c9e13c8c5845f209cb56c749203df72b5
ZACK_Im <adolfobaradat@gmail.com>
2026-01-08 10:54:28 -0400

    Update CMDCommandAnalyzer V1.ps1

--- CMDCommandAnalyzer V1.ps1
+++ CMDCommandAnalyzer V1.ps1
@@ -11,7 +11,7 @@ $genLog = Join-Path $logDir "ComandosGenerales.txt"
 
 # Comandos sospechosos
 $suspiciousPatterns = @(
-    "fsutil", "sc stop", "reg delete", "assign letter", "type", "-framerate", "powershell", "iwr", "iex", "invoke-"
+    "fsutil", "sc stop", "reg delete", "assign letter", "type", "-framerate", "powershell", "iwr", "iex", "invoke-", "github"
 )
 
 # Procesos a analizar
